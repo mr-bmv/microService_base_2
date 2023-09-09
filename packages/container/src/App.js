7b1c523abbdb9e7fd622c7490484f3903e77fa9f
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-09 15:08:00 +0300

    Добавлена уникальность классов для CSS

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// нужен для обеспечения уникальности css классов при сборке продакшена
+// так как каждое отдельное приложение будет минифицированно по одинаковым
+// правилам, то есть вероятность, то часть классов в дочерних приложения
+// объединяться при их запуске через родительское
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div >
-    </BrowserRouter >
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div >
+      </BrowserRouter >
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// нужен для обеспечения уникальности css классов при сборке продакшена
+// так как каждое отдельное приложение будет минифицированно по одинаковым
+// правилам, то есть вероятность, то часть классов в дочерних приложения
+// объединяться при их запуске через родительское
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
